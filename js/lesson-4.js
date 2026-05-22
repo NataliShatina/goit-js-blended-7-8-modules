3ba4d06ff7cd9afd940dc168a410ad4633d21521
Natali Shatina <natali.shatina@gmail.com>
2026-05-22 16:44:31 +0200

    creating repo

---
+++ js/lesson-4.js
@@ -0,0 +1,5 @@
+// 1 - отримай body елемент і виведи його в консоль;
+// const body = document.querySelector("body");
+// console.log(body);
+
+// 2 - отримай елемент id="title" і виведи його в консоль;
