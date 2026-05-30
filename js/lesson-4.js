228ae561b5b8310bf0382d1fc5b14e7a43165cbc
Natali Shatina <natali.shatina@gmail.com>
2026-05-30 15:02:01 +0200

    task three

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -130,4 +130,59 @@ list.insertAdjacentHTML("beforeend", newItem);
 // Парні числа повинні мати зелений фон (додати клас even),
 // Непарні числа - жовтий фон (додати клас odd).
 
-const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+// const numbContainer = document.querySelector(".number-container");
+
+// for (let index = 0; index < 100; index++) {
+//   const div = document.createElement("div");
+//   const number = randomNumber();
+//   div.textContent = number;
+//   div.classList.add("number");
+//   if (number % 2 === 0) {
+//     div.classList.add("even");
+//   } else {
+//     div.classList.add("odd");
+//   }
+//   numbContainer.append(div);
+// }
+
+// !===========TASK 3===================
+// Form Events, Input, Focus, Blur and Submit.
+
+// Використовуй шаблон форми з файлу html.
+
+// 1 - При події `input`, якщо користувач ввів в поле більше
+// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// то клас `error`
+const input = document.querySelector(".js-username-input");
+// console.log(input);
+input.addEventListener("input", () => {
+  const value = input.value.trim();
+  // console.log(value);
+  if (value.length > 6) {
+    input.classList.add("success");
+    input.classList.remove("error");
+  } else {
+    input.classList.add("error");
+    input.classList.remove("success");
+  }
+});
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку,
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також при події інпут реалізуй додавання
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видалив, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення
+// false, верни дефолтне значення "Anonymous" у span.
